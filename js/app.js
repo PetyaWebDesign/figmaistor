b1e0445c5da69b59c7a11062366f45b827128afd
Petya Petkova <petyawebdesign@gmail.com>
2025-09-12 17:56:38 +0300

    add GSAP, add sticky header

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
+// Sticky header
+const header = document.getElementById('header');
+let lastScrollY = window.scrollY;
+let isSticky = true; // header is visible at start
+
+// Ensure header is visible initially
+gsap.set(header, { y: 0, opacity: 1 });
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY < lastScrollY) {
+        // scrolling up
+        if (!isSticky) {
+            gsap.to(header, { y: 0, opacity: 1, duration: 0.4, ease: "power2.out" });
+            isSticky = true;
+        }
+    } else {
+        // scrolling down
+        if (isSticky) {
+            gsap.to(header, { y: -80, opacity: 0, duration: 0.4, ease: "power2.in" });
+            isSticky = false;
+        }
+    }
+    lastScrollY = window.scrollY;
+});
+
+
+
+
+
+// Home page hero slider
 const swiper = new Swiper('.swiper', {
     // Optional parameters
     loop: true,
@@ -11,7 +41,4 @@ const swiper = new Swiper('.swiper', {
     pagination: {
         el: '.swiper-pagination',
     },
-    
-
-
 });
